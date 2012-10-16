e23012ff1cd00582f29ee5bba2836167771458b9
Craig Walls <cwalls@vmware.com>
2012-10-16 08:31:23 -0500

    Sync

--- thorax-lumbar-client/src/main/thorax/js/views/s2gx/Sessions.js
+++ thorax-lumbar-client/src/main/thorax/js/views/s2gx/Sessions.js
@@ -1,4 +1,12 @@
 S2GX.View.extend({
-  name: 's2gx/Sessions'
+  name: 's2gx/Sessions',
+  events: {
+	  "click .listitem" : "sessionClicked"
+  },
+  
+  sessionClicked : function(e) {
+	  sessionid = e.currentTarget.attributes[0].value;
+  }
+
 });
  
