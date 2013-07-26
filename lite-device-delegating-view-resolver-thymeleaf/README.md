c709f604f3ee7eecd50d9bb3479aa2d364a568a6
Roy Clarkson <rclarkson@gopivotal.com>
2013-07-26 17:31:53 -0400

    Add LiteDeviceDelegatingViewResolver sample using Thymeleaf

---
+++ lite-device-delegating-view-resolver-thymeleaf/README.md
@@ -0,0 +1,27 @@
+# lite-device-delegating-view-resolver-thymeleaf
+
+A [Spring Mobile] sample application demonstrating many of the capabilities of the Spring Mobile Device module, including:
+
+* "Lite" device resolution through the use of DeviceResolverHandlerInterceptor 
+* Site preference management
+* View resolution with LiteDeviceDelegatingViewResolver
+* Thymeleaf integration
+
+
+## Build and Deploy
+
+1. See the main [README](../README.md) for build instructions.
+
+2. Access the sample application at the following URL:
+
+    <http://localhost:8080/lite-device-delegating-view-resolver-thymeleaf>
+
+    > **Note**: access the site from your browser, Android emulator or iOS simulator to demonstrate the functionality.
+
+
+## License
+
+[Spring Mobile] is released under version 2.0 of the [Apache License].
+
+[Spring Mobile]: http://www.springsource.org/spring-mobile
+[Apache License]: http://www.apache.org/licenses/LICENSE-2.0
---
+++ lite-device-delegating-view-resolver-thymeleaf/src/main/java/org/springframework/showcases/lite/config/WebConfig.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2010-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.showcases.lite.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.view.LiteDeviceDelegatingViewResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+/**
+ * @author Roy Clarkson
+ * @see WebMvcConfigurer
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.springframework.showcases.lite")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	// implementing WebMvcConfigurer
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new DeviceResolverHandlerInterceptor());
+		registry.addInterceptor(new SitePreferenceHandlerInterceptor());
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(new SitePreferenceHandlerMethodArgumentResolver());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	// additional webmvc-related beans
+
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/WEB-INF/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
+		return resolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+
+	@Bean
+	public LiteDeviceDelegatingViewResolver liteDeviceDelegatingViewResolver() {
+		ThymeleafViewResolver delegate = new ThymeleafViewResolver();
+		delegate.setTemplateEngine(templateEngine());
+		delegate.setOrder(1);
+		LiteDeviceDelegatingViewResolver resolver = new LiteDeviceDelegatingViewResolver(delegate);
+		resolver.setMobilePrefix("mobile/");
+		resolver.setTabletPrefix("tablet/");
+		return resolver;
+	}
+
+}
