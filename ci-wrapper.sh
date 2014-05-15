d17266e347fe38d50db095083d30a79400a396f8
Roy Clarkson <rclarkson@gopivotal.com>
2014-05-15 17:03:20 -0500

    Upgrade to Gradle 1.12

--- ci-wrapper.sh
+++ ci-wrapper.sh
@@ -7,13 +7,14 @@ function wrapper {
     cd ../
 }
 
-wrapper lite-device-delegating-view-resolver
-wrapper lite-device-delegating-view-resolver-thymeleaf
+wrapper lite-device-delegating-view-resolver-jc
+wrapper lite-device-delegating-view-resolver-jc-thymeleaf
 wrapper lite-device-delegating-view-resolver-xml
 wrapper lite-device-resolver
+wrapper lite-device-resolver-jc
 wrapper lite-device-resolver-xml
-wrapper lite-site-preference-handler
-wrapper lite-site-switcher-handler-mdot
-wrapper lite-site-switcher-handler-urlpath
+wrapper lite-site-preference-handler-jc
+wrapper lite-site-switcher-handler-jc-mdot
+wrapper lite-site-switcher-handler-jc-urlpath
 
 exit
