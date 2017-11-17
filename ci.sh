8ab859e26e0297c84d5072ab034d3123952216dd
Roy Clarkson <roy@clarkson.org>
2017-11-17 14:35:32 -0500

    Disable build for archived projects

--- ci.sh
+++ ci.sh
@@ -10,16 +10,16 @@ function build {
 }
 
 build lite-device-resolver
-build lite-site-preference-handler-jsp
+# build lite-site-preference-handler-jsp
 build lite-device-delegating-view-resolver
-build lite-device-delegating-view-resolver-jsp
-build archive/java-configuration/lite-device-resolver-jc
-build archive/java-configuration/lite-site-preference-handler-jc
-build archive/java-configuration/lite-device-delegating-view-resolver-jc
-build archive/java-configuration/lite-device-delegating-view-resolver-jc-thymeleaf
-build archive/java-configuration/lite-site-switcher-handler-jc-mdot
-build archive/java-configuration/lite-site-switcher-handler-jc-urlpath
-build archive/xml-configuration/lite-device-resolver-xml
-build archive/xml-configuration/lite-device-delegating-view-resolver-xml
+# build lite-device-delegating-view-resolver-jsp
+# build archive/java-configuration/lite-device-resolver-jc
+# build archive/java-configuration/lite-site-preference-handler-jc
+# build archive/java-configuration/lite-device-delegating-view-resolver-jc
+# build archive/java-configuration/lite-device-delegating-view-resolver-jc-thymeleaf
+# build archive/java-configuration/lite-site-switcher-handler-jc-mdot
+# build archive/java-configuration/lite-site-switcher-handler-jc-urlpath
+# build archive/xml-configuration/lite-device-resolver-xml
+# build archive/xml-configuration/lite-device-delegating-view-resolver-xml
 
 exit
